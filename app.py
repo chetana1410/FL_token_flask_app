363f4d641fa7550784bb6eb4e28ff7a88612f2cd
chetana1410 <64744226+chetana1410@users.noreply.github.com>
2021-12-08 16:11:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask , render_template , redirect , url_for , request
+#from create_plan import user_inputs
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    
+    return render_template("index.html")
+
+
+# @app.route('/submit',methods = ['POST','GET'])
+# def submit():
+    
+#     if request.method=='POST':
+#         return user_inputs(request.form)
+    
+    
+    
+    
---
+++ create_plan.py
@@ -0,0 +1,50 @@
+from brownie import FELToken, ProjectContract, accounts, config, network
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    fund_with_link,
+    get_account,
+    get_contract,
+)
+from sklearn.linear_model import LinearRegression 
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+
+
+from felt.builder import upload_model
+
+def user_inputs(form):
+    
+    model_type = form["model"]
+    rounds = int(form["rounds"])
+    tokens = int(form["tokens"])
+    address = form["Address"]
+    
+    def create_plan(project, builder):
+        # Add LINK to contract for testing
+        fund_with_link(address)
+
+        ## DEFINE MODEL ###
+        if model_type=='linear':
+            model = LinearRegression()
+        elif model_type=='svm': 
+            model = SVR(kernel=form(["kernel"]))
+        elif model_type=='random forest':
+            model = RandomForestRegressor()
+            
+        cid = upload_model(model)
+
+        ### PROVIDE REWARDS AND UPLOAD PLAN ###
+        FELToken[-1].increaseAllowance(address, 1000, {"from": builder})
+        requestId = project.createPlan(cid, rounds, tokens, {"from": builder})
+
+        # Simulate the VRF on local chain
+        if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+            get_contract("vrf_coordinator").callBackWithRandomness(
+                requestId.return_value, 777, address, {"from": get_account()}
+            )
+
+
+def main():
+    project = ProjectContract[-1]
+    builder = accounts.add(config["wallets"]["owner_key"])
+    create_plan(project, builder)
